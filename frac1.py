d5725045c1a243f3845d27d8d5e7f65ea0a88b11
daivikj <daivikj4@gmail.com>
2019-02-20 18:52:54 +0530

    20/02/19

---
+++ frac1.py
@@ -0,0 +1,7 @@
+def addFrac(num1, den1, num2, den2):
+    num = (num1 * den2) + (num2 * den1)
+    den = den1 + den2
+    return(num, den)
+
+(num, den) = addFrac(5, 2, 6, 7)
+print(str(num) + '/' + str(den))
